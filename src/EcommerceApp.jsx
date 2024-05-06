30323afbaad3ce3c066f46540435eb3577130cb4
JErickDev <jerickgm89@gmail.com>
2024-05-06 16:55:33 -0500

    Config: Configuracion de las dependencias y organizacion de las carpetas
    Se instalo con bun React Router, Material UI, y se configuro la paleta de colores con la fuente de Roboto

--- src/EcommerceApp.jsx
+++ src/EcommerceApp.jsx
@@ -1,8 +1,12 @@
+import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme"
 
 export const EcommerceApp = () => {
   return (
     <>
-        <h1>Hola Equipo Frontend</h1>
+      <AppTheme>
+        <AppRouter />
+      </AppTheme>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { EcommerceApp } from './EcommerceApp.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 // import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <EcommerceApp />
+    <BrowserRouter>
+      <EcommerceApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
