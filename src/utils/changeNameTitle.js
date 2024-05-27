adf48f3996102f239a137b10fe8c4b71289b5a63
JErickDev <jerickgm89@gmail.com>
2024-05-27 00:14:06 -0500

    Feat: Se agrego una pequena pero divertida funcionalidad para la pestana del navegador
    Si lees este comentario escribeme y dime si lei todo tu commit

---
+++ src/utils/changeNameTitle.js
@@ -0,0 +1,10 @@
+let previousTitle = document.title;
+
+window.addEventListener('blur', () => {
+    previousTitle = document.title;
+    document.title = '¡No te vayas! ¡Vuelve! 🥺';
+})
+
+window.addEventListener('focus', () => {
+    document.title = previousTitle;
+} )
