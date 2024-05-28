fe890b4ad432193fd9e8fd4e19486cdebcec6024
JErickDev <jerickgm89@gmail.com>
2024-05-28 01:09:35 -0500

    Feat: Admin ahora se muestra un campo que lista las preguntas de los productos
    Aun falta implementar la respuesta y que lo muestre en la pagina

--- src/admin/components/ListQuestions.jsx
+++ src/admin/components/ListQuestions.jsx
@@ -1,7 +1,6 @@
-import { useGetUsersByIdsQuery }  from "../../hooks/useGetUsersByIdsQuery"
+import { useGetProductsQuery }  from "../../hooks/useGetProductsByIdsQuery"
 import { DataGrid } from '@mui/x-data-grid'
 import { useGetQuestionsQuery, usePutUpdateQuestionMutation } from '../../store/api/ecommerceQuestionsApi'
-import { useGetProductByIdQuery } from '../../store/api'
 import { useNavigate } from 'react-router-dom'
 import {Delete as DeleteIcon, Edit as EditIcon} from '@mui/icons-material'
 import { Box, IconButton, Typography } from '@mui/material'
@@ -12,13 +11,50 @@ export const ListQuestions = () => {
     console.log(questions)
     const idProductQuestion = [...new Set(questions.map(question => question.idProduct))];
     console.log(idProductQuestion);
-    const { data: productData = [], error: errorProduct, isLoading: isLoadingProduct } = useGetProductByIdQuery(idProductQuestion);
-    console.log(productData);
+    const { data: products = [], error: errorProduct, isLoading: isLoadingProduct } = useGetProductsQuery(idProductQuestion);
+    console.log(products);
 
     const columns = [
-        {field: 'id', headerName: 'ID', minWidth: 50, flex: 1},
+        {field: 'id', headerName: 'ID', maxWidth: 80, flex: 1},
         {field: 'comments', headerName: 'Pregunta', minWidth: 150, flex: 1},
-        {field: 'actions', headerName: 'Eliminar', minWidth: 100, flex: 1, renderCell: (params) => {
+        {
+            field: 'response', 
+            headerName: 'Respuesta', 
+            minWidth: 150, 
+            flex: 1,
+            renderCell: (params) => {
+                return params.value ? params.value : <span style={{ color: 'red' }}>Por contestar pregunta</span>;
+            }
+        },
+        {
+            field: 'productImage', 
+            headerName: 'Imagen del Producto', 
+            maxWidth: 150, 
+            flex: 1, 
+            renderCell: (params) => {
+                const product = products.find(p => p.idProduct === params.row.productId);
+                if (product) {
+                    return (
+                        <div style={{ display: 'flex', justifyContent: 'center' }}>
+                            <img src={product.imageProducts[0]} alt={product.nameProduct} style={{width: '50px', height: '50px'}}/>
+                        </div>
+                    );
+                } else {
+                    return 'Imagen no disponible';
+                }
+            }
+        },
+        {
+            field: 'productName', 
+            headerName: 'Nombre del Producto', 
+            minWidth: 150, 
+            flex: 1, 
+            renderCell: (params) => {
+                const product = products.find(p => p.idProduct === params.row.productId);
+                return product ? product.nameProduct : 'Producto no encontrado';
+            }
+        },
+        {field: 'actions', headerName: 'Eliminar', maxWidth: 150, flex: 1, renderCell: (params) => {
             const question = questions.find(p => p.idQuestion === params.id)
             return (
                 <>
@@ -37,6 +73,8 @@ export const ListQuestions = () => {
         return {
             id: question.idComments,
             comments: question.comments,
+            response: question.responseComments,
+            productId: question.idProduct,
         }
     })
   return (
