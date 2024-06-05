8e5f8ceacc730ae1cdc0ada504a7a8ac0cd5f4d6
JErickDev <jerickgm89@gmail.com>
2024-06-04 23:52:41 -0500

    Feat: Se implemento el menu flotante para pantallas pequenas en PanelUser

--- src/userPanel/components/SideBarUser.jsx
+++ src/userPanel/components/SideBarUser.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
-import { Box, Grid, List, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material"
+import { Stack, Box, Grid, List, ListItemButton, ListItemIcon, ListItemText, Typography, Drawer, Fab, useTheme, useMediaQuery, Divider } from "@mui/material"
 import { 
   ShoppingBagOutlined as ShoppingBagIcon,
   FavoriteBorderOutlined as FavoriteIcon,
   Place as PlaceIcon,
   Payment as PaymentIcon,
   Person as PersonIcon,
+  Menu as MenuIcon,
 } from "@mui/icons-material"
+
 
 const ListItemLink = ({ to, icon: Icon, primary }) => {
   const location = useLocation();
   const isActive = location.pathname === to;
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <NavLink to={to} style={{ textDecoration: 'none', color: 'inherit' }}>
@@ -35,37 +40,83 @@ const ListItemLink = ({ to, icon: Icon, primary }) => {
 };
 
 export const SideBarUser = () => {
+    const [open, setOpen] = useState(false);
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <Grid 
-      item 
-      xs={12} 
-      md={12}
-      lg={4}
-      sx={{display: 'flex', justifyContent: 'right', alignItems: 'flex-start'}}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 2,
-          m: 4,
-          p: 5,
-          borderRadius: 4,
-          backgroundColor: '#fff',
-          width: { xs: '100%'}
-        }}
-      >
-        <List component="div">
-          <Typography variant="h6">DashBoard</Typography>
-          <ListItemLink to="/user/order" icon={ShoppingBagIcon} primary="Ordenes" />
-          <ListItemLink to="/user/wishlist" icon={FavoriteIcon} primary="Lista de deseos" />
-        </List>
-        <List component="div">
-          <Typography variant="h6">Configuracion de Perfil</Typography>
-          <ListItemLink to="/user" icon={PersonIcon} primary="Información personal" />
-          <ListItemLink to="/user/address" icon={PlaceIcon} primary="Direcciones" />
-        </List>
-      </Box>
-    </Grid>
+    <>
+      {isSmallScreen ? (
+        <>
+          <Fab 
+            color="primary" 
+            aria-label="add" 
+            onClick={() => setOpen(!open)}
+            sx={{
+              position: isSmallScreen ? 'fixed' : 'relative',
+              bottom: isSmallScreen ? 20 : 'auto',
+              right: isSmallScreen ? 20 : 'auto',
+            }}
+          >
+            <MenuIcon />
+          </Fab>
+          <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+            <Grid 
+              item 
+              xs={12} 
+              md={12}
+              lg={4}
+              sx={{display: 'flex', justifyContent: 'right', alignItems: 'flex-start'}}
+            >
+              <Stack direction="column" spacing={2}>
+                <List component="div">
+                  <Typography variant="h6" sx={{ml:2}}>DashBoard</Typography>
+                  <ListItemLink to="/user/order" icon={ShoppingBagIcon} primary="Ordenes" />
+                  <ListItemLink to="/user/wishlist" icon={FavoriteIcon} primary="Lista de deseos" />
+                </List>
+                <Divider />
+                <List component="div">
+                  <Typography variant="h6" sx={{ml:2}}>Configuracion de Perfil</Typography>
+                  <ListItemLink to="/user" icon={PersonIcon} primary="Información personal" />
+                  <ListItemLink to="/user/address" icon={PlaceIcon} primary="Direcciones" />
+                </List>
+              </Stack>
+            </Grid>
+          </Drawer>
+        </>
+      ) : (
+        <Grid 
+          item 
+          xs={12} 
+          md={12}
+          lg={4}
+          sx={{display: 'flex', justifyContent: 'right', alignItems: 'flex-start'}}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+              m: 4,
+              p: 5,
+              borderRadius: 4,
+              backgroundColor: '#fff',
+              width: { xs: '100%'}
+            }}
+          >
+            <List component="div">
+              <Typography variant="h6">DashBoard</Typography>
+              <ListItemLink to="/user/order" icon={ShoppingBagIcon} primary="Ordenes" />
+              <ListItemLink to="/user/wishlist" icon={FavoriteIcon} primary="Lista de deseos" />
+            </List>
+            <List component="div">
+              <Typography variant="h6">Configuracion de Perfil</Typography>
+              <ListItemLink to="/user" icon={PersonIcon} primary="Información personal" />
+              <ListItemLink to="/user/address" icon={PlaceIcon} primary="Direcciones" />
+            </List>
+          </Box>
+        </Grid>
+      )}
+    </>
   )
 }
